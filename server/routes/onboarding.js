4b4df3328c8d9d9f59886f419783d88327ac71ea
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2025-12-14 22:37:05 +0100

    query for inserting userdata into two different tables containing the id

--- server/routes/onboarding.js
+++ server/routes/onboarding.js
@@ -5,21 +5,19 @@ import pool from "../db.js"
 const onboarding_router = express.Router()
 
 
-
-
-
-
 onboarding_router.post("/postData", async (req, res) => {
     const name = req.body.name
     const onboarding = 'onboarding'
     console.log(name)
+    
+    // const create_form = 'INSERT INTO employee_forms (user_id, form_type) VALUES ( (SELECT id FROM users ORDER BY id DESC LIMIT 1) , $1)'
+    // const create_form = 'INSERT INTO employee_forms (user_id, form_type) VALUES (SELECT MAX(id) from users, $1)'
+
+    const creating_user = 
+    'WITH getval(id) as (INSERT INTO users (name) VALUES ($1) RETURNING id) INSERT INTO employee_forms (user_id, form_type) VALUES((SELECT id from getval),  $2);' 
 
 
-    const creating_user = 'INSERT INTO users(name) VALUES ($1)'
-    const create_form = 'INSERT INTO employee_forms (user_id, form_type) VALUES ( (SELECT id FROM users ORDER BY id DESC LIMIT 1) , $1)'
-
-
-    pool.query(creating_user, [name], async (err, result) => {
+    pool.query(creating_user, [name, onboarding], async (err, result) => {
         if(err) {
             res.send(err)
             console.log(err)
@@ -28,20 +26,29 @@ onboarding_router.post("/postData", async (req, res) => {
         }
     })
 
- 
-    pool.query(create_form, [onboarding], async (err, result) => {
-        if(err) {
+    // pool.query(create_form, [onboarding], async (err, result) => {
+    //     if(err) {
 
-            console.log(err)
-        } else{
-            console.log(result)
-        }
-    })
+    //         console.log(err)
+    //     } else{
+    //         console.log(result)
+    //     }
+    // })
 
-
-
-
+    // pool.query(create_form, async (err, result) => {
+    //     if(err) {
+    //         res.send(err)
+    //         console.log(err)
+    //     } else{
+    //         console.log(result)
+    //     }
+    // })
 })
+
+
+
+
+
 
 export default onboarding_router;
 
